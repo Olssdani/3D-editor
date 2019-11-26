5e01df18e4f7ab255f54722a387e7e09711321cb
Olssdani <daniel.olsson1992@gmail.com>
2019-11-26 08:50:31 +0100

    Swithching computers

--- G-E/G-E/Source/Transformations/transformationObject.h
+++ G-E/G-E/Source/Transformations/transformationObject.h
@@ -1,17 +1,30 @@
 #pragma once
 #include "glm/glm.hpp"
+#include <cmath>
 #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/gtc/quaternion.hpp> 
 #include <glm/gtx/quaternion.hpp>
 class transformationObject {
 public:
 	transformationObject() {
-
+		position = glm::vec4(0.0f);
+		rotationX = glm::fquat(1.0f, 0, 0, 0);
+		rotationY = glm::fquat(1.0f, 0, 0, 0);
+		glm::vec3 direction = glm::vec3(0.0f);
+	}
+	//TODO Implement setup for initial position
+	transformationObject(glm::vec3 startPosition) {
+		position2Components(startPosition);
 	}
 
 
-	void rotate(float angleX, float angleY, float angleZ) {
+	void rotate(float angleX, float angleY) {
+		
+		glm::vec3 angle = glm::vec3(angleX, 0, 0);
+		rotationX = rotationX * glm::fquat(angle);
 
+		angle = glm::vec3(0, angleY, 0);
+		rotationY = rotationY * glm::fquat(angle);
 	}
 
 	void translate() {
@@ -22,10 +35,31 @@ public:
 		return glm::vec3(position);
 	}
 
+	
 
 private:
-	glm::vec4 position;u
+	glm::vec4 position;
 	glm::fquat rotationX, rotationY;
 	float offset;
 	glm::vec3 direction;
+
+
+	void update() {
+
+	}
+
+	//Allocate the correct value for all components from a startposition.
+	void position2Components(glm::vec3 startPosition) {
+		offset = glm::length(startPosition);
+		direction = glm::normalize(startPosition);
+		float theta = atan(startPosition.y / startPosition.x);
+		float phi = acos(startPosition.z / startPosition.r);
+		
+		glm::vec3 angle = glm::vec3(theta, 0, 0);
+		rotationX = rotationX * glm::fquat(angle);
+
+		angle = glm::vec3(0, phi, 0);
+		rotationY = rotationY * glm::fquat(angle);
+		
+	}
 };
